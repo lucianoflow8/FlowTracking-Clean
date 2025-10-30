e1b5857e3c8e1f206b282da417bd3c9ceb75b9a1
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-10-29 21:06:28 -0300

    chore: enable Next standalone output

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
-  output: "standalone",
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
